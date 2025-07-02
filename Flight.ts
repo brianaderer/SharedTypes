77d0e4fd2f74fc538d6d4a513ef13eabae95249b
Brian Aderer <brian@brianaderer.com>
2025-07-01 21:40:56 -0400

    add Flight to SharedTypes

---
+++ Flight.ts
@@ -0,0 +1,13 @@
+import {Item} from '../Item';
+export interface Flight {
+  id: null | string;
+  wines: Array<string>;
+  selectedWine: Item;
+  name: string;
+  currentVersion: null | number;
+  versions: Item;
+  owner: string;
+  rep_contact: Item | null;
+  timestamp: string | number;
+  distributorName: string | null;
+}
